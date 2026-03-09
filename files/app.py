90e49aa97d66c0bb8e912689b68fd6b40eab1da6
Khang <vt427@drexel.edu>
2026-03-09 02:00:13 -0400

    Add password reset feature and environment variable setup

--- files/app.py
+++ files/app.py
@@ -34,6 +34,7 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
 
+app.config["MAIL_DEFAULT_SENDER"] = app.config["MAIL_USERNAME"]
 mail = Mail(app)
 
 # CSRF Configuration for production
